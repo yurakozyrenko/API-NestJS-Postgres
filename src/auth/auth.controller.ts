be62ef959b0ccd923cd68c231b20ee71355da2ce
Yura <96086557+yurakozyrenko@users.noreply.github.com>
2023-12-06 14:56:08 +0300

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,18 @@
 import { Controller, Body, Post } from '@nestjs/common';
-import { CreateUserDto } from '../users/dto/create-user.dto';
+import { AuthUserDto } from '../users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { User } from 'src/users/users.model';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger/dist';
 
 @ApiTags('Авторизация')
 @Controller('auth')
- 
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @ApiOperation({ summary: 'авторизация пользователя' })
   @ApiResponse({ status: 200, type: User })
   @Post('/login')
-  login(@Body() userDto: CreateUserDto) {
-    return this.authService.login(userDto);
+  login(@Body() authDto: AuthUserDto) {
+    return this.authService.login(authDto);
   }
-
-  //   @ApiOperation({ summary: 'Создание пользователя' })
-  //   @ApiResponse({ status: 200, type: User })
-  // @Post('/registration')
-  // registration(@Body() userDto: CreateUserDto) {
-  //   return this.authService.registration(userDto);
-  // }
 }
